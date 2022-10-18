c09b2dc3773627bcfe681024dd01fea5b32bc357
audrey <audrey.murigi@gmail.com>
2022-10-18 14:21:13 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -15,6 +13,6 @@
 int main(void)
 
 {  
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
